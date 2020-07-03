b1f73740789ae61b4c9bd907f604d9fa723150e0
Matthew Sprague <matt.sprague@gmail.com>
2020-07-03 11:36:40 -0700

    links: basic link detection code

--- backend/src/cmd/parse_message.ts
+++ backend/src/cmd/parse_message.ts
@@ -1,5 +1,5 @@
 import { gmail_v1 } from 'googleapis';
-import { parse } from 'node-html-parser';
+import { parse, HTMLElement } from 'node-html-parser';
 import { Context } from '../context';
 
 export const parseEmail = (context: Context, message: gmail_v1.Schema$Message) => {
@@ -9,17 +9,47 @@ export const parseEmail = (context: Context, message: gmail_v1.Schema$Message) =
     return {};
 };
 
-// @ts-ignore
-const parseMessagePart = (context: Context, messagePart: gmail_v1.Schema$MessagePart) => {
-    return messagePart.parts.map((part) => {
+type DetectedLink = {
+    type: string;
+    href: string;
+    firstCharPos: number;
+};
+
+const linkAnalyzer = (urlRegex: RegExp, linkElement: HTMLElement): string => {
+    const href = linkElement.getAttribute('href');
+    if (!urlRegex.exec(href)) {
+        return undefined;
+    }
+    return href;
+};
+
+const linkAnalysisConfigs = [
+    {
+        type: 'googleDrive',
+        urlRegex: /http/,
+    },
+];
+
+const parseMessagePart = (context: Context, messagePart: gmail_v1.Schema$MessagePart): Array<DetectedLink> => {
+    return messagePart.parts.flatMap((part) => {
         if (part.mimeType === 'text/html') {
             const buff = new Buffer(part.body.data, 'base64');
             const text = buff.toString('ascii');
             const rootElement = parse(text);
             const links = rootElement.querySelectorAll('a');
-            return links.map((link) => link.getAttribute('href'));
+            const detectedLinks = linkAnalysisConfigs.flatMap((config) =>
+                links
+                    .map((link) => linkAnalyzer(config.urlRegex, link))
+                    .filter(Boolean)
+                    .map(
+                        (match) =>
+                            ({ href: match, type: config.type, firstCharPos: text.indexOf(match) } as DetectedLink),
+                    ),
+            );
+            return detectedLinks;
         } else if (part.mimeType === 'multipart/mixed' || part.mimeType === 'multipart/related') {
-            return part.parts.map((part: gmail_v1.Schema$MessagePart) => parseMessagePart(context, part));
+            return part.parts.flatMap((part: gmail_v1.Schema$MessagePart) => parseMessagePart(context, part));
         }
+        return [];
     });
 };
